83d8af67c8a9d9b73e22dd905d78ee27c082f37e
vasudev <vasudevkumar1445@gmail.com>
2026-03-07 20:03:37 +0530

    Edit sudokoGame.js

---
+++ examples/sampleGame.js
@@ -0,0 +1,19 @@
+## Sudoku JSON
+
+{
+  "id": "game001",
+  "type": "sudoku",
+  "title": "Sudoku Challenge",
+  "difficulty": "easy",
+  "timeLimit": 300,
+  "gridSize": 9,
+  "initialGrid": [
+    [5,3,0,0,7,0,0,0,0],
+    [6,0,0,1,9,5,0,0,0],
+    [0,9,8,0,0,0,0,6,0]
+  ],
+  "scoring": {
+    "correctCell": 10,
+    "bonusTime": 5
+  }
+}
